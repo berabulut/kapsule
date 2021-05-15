1abbe61fec32b76d2d9324d63412ba9fefee4ceb
Hüseyin Bera Bulut <berabulut@gmail.com>
2021-05-15 20:26:40 +0300

    unit test for title package

--- handlers/geo/geo_test.go
+++ handlers/geo/geo_test.go
@@ -2,12 +2,24 @@ package geo
 
 import "testing"
 
+type addTest struct {
+	ip, country_code string
+}
+
+var tests = []addTest{
+	{"1.1.1.1", "AUS"},
+	{"4.1.1.2", "USA"},
+}
+
 func TestGetCountryCode(t *testing.T) {
 
-	got, _ := GetCountryCode("1.1.1.1")
-	want := "AUS"
+	for _, test := range tests {
+		got, _ := GetCountryCode(test.ip)
+		want := test.country_code
 
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
+		if got != want {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
 	}
+
 }
