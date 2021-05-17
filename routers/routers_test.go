d29d5b60af1511194bff96f54e7707bb9339bd9b
Hüseyin Bera Bulut <berabulut@gmail.com>
2021-05-17 17:01:01 +0300

    add unit tests

--- routers/routers_test.go
+++ routers/routers_test.go
@@ -67,5 +67,117 @@ func TestRedirectURL(t *testing.T) {
 		}
 
 	}
+}
 
+func TestShortenURL(t *testing.T) {
+	r := ApiRouter(records)
+
+	type addTest struct {
+		key   string
+		link  string
+		title string
+	}
+
+	var tests []addTest
+
+	for _, record := range records {
+		tests = append(tests, addTest{
+			key:   record.Key,
+			link:  record.Value,
+			title: record.Title,
+		})
+	}
+
+	for _, test := range tests {
+
+		body := strings.NewReader(fmt.Sprintf(`{"url": "%s"}`, test.link))
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/shorten", body)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		r.ServeHTTP(w, req)
+
+		want := true
+		got := strings.Contains(w.Body.String(), test.title)
+
+		if got != want {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
+
+		got = strings.Contains("200", fmt.Sprintf("%v", w.Code))
+
+		if got != want {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
+	}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/shorten", strings.NewReader(`{"url": "notgonnawork"}`))
+	r.ServeHTTP(w, req)
+
+	got := w.Code
+	want := 500
+
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
+
+func TestGetDetails(t *testing.T) {
+	r := ApiRouter(records)
+
+	type addTest struct {
+		key   string
+		link  string
+		title string
+	}
+
+	var tests []addTest
+
+	for _, record := range records {
+		tests = append(tests, addTest{
+			key:   record.Key,
+			link:  record.Value,
+			title: record.Title,
+		})
+	}
+
+	for _, test := range tests {
+		url := fmt.Sprintf("/%s", test.key)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", url, nil)
+		r.ServeHTTP(w, req)
+
+		got := strings.Contains("200", fmt.Sprintf("%v", w.Code))
+		want := true
+
+		if got != want {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
+
+		got = strings.Contains(w.Body.String(), test.link)
+
+		if got != want {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
+
+		got = strings.Contains(w.Body.String(), test.title)
+
+		if got != want {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
+
+	}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/abcdefsdg", nil)
+	r.ServeHTTP(w, req)
+
+	got := w.Code
+	want := 404
+
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
 }
