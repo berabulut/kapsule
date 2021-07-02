026012029d8e4b9e9db5a281188a39e1fa675eee
Hüseyin Bera Bulut <berabulut@gmail.com>
2021-07-02 10:27:40 +0300

    import assets from GitHub repo @[build]

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ docker build  -t kapsule-api -f ./Dockerfile-api .
 docker build  -t kapsule-redirect -f ./Dockerfile-redirect .
 
 docker run -d -i -t -p 8080:8080 kapsule-api
-docker run -d -i -t -p 8081:8081 kapsule-redirect
+docker run -d -i -t -p 8081:8080 kapsule-redirect
